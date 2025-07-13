15f6782058b4e9b38c2af2ba2d69ab6f497d19a7
preciousekele <preshekele@gmail.com>
2025-07-13 16:12:16 +0100

    removed unused font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import StarsCanvas from "@/component/main/StarBackground";
